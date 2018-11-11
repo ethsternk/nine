681431e9ca073cfdd955f3178555418f44f5b22c
Ethan Sternke <ethan.sternke@gmail.com>
2018-11-10 22:39:39 -0500

    base functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillMount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: Date()
+    })
+  }
+
+  convertDate(date) {
+    let hours = [parseInt(date[16]), + parseInt(date[17])]
+    let minutes = [parseInt(date[19]), + parseInt(date[20])]
+    if ((hours[0] === 1 && hours[1] > 2) || (hours[0] === 2)) {
+      hours[0] -= 1
+      hours[1] -= 2
+    }
+    let sum = hours[0] + hours[1] + minutes[0] + minutes[1]
+    if (sum >= 10) {
+      sum = String(sum)
+      sum = parseInt(sum[0]) + parseInt(sum[1])
+    }
+    if (sum === hours[0] || sum === hours[1] || sum === minutes[0] || sum === minutes[1]) {
+      return "True"
+    } else {
+      return "False"
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>{this.convertDate(this.state.date)}</p>
       </div>
-    );
+    )
   }
 }
 
